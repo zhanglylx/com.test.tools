294f6f950614ca2838fa9eb9e980c0193def60e2
张连宇HP <zhanglylx@163.com>
2018-07-31 20:24:46 +0800

    更改松鼠config名称

---
+++ src/java/SquirrelFrame/SquirrelConfig.java
@@ -0,0 +1,13 @@
+package SquirrelFrame;
+
+import java.awt.*;
+import java.io.File;
+
+public class SquirrelConfig {
+    public static final String guanyu = "v2.1.0 更新内容\n  1.取消获取广告日志中的计时时间\n" +
+            "  2.加入截取手机屏幕功能,抓取图片更方便\n  3.清理手机文件加入清除data中的包名文件" ;
+    public static final String TOOLSTITLE = "松鼠  v2.1.0";
+    public static final String OPENWAIT="文件较大，请等待";
+    public static final String Screenshot_save_path = "image"+File.separator+"screen"+File.separator; //录屏与截图保存地址
+    public static final Font typeface = new Font("标楷体",Font.BOLD, 15);
+}
