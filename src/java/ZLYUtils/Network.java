eb8a87b77c0fd05689bcf262cd6e5a41cbf9f7dc
张连宇HP <zhanglylx@163.com>
2018-08-03 16:17:12 +0800

    接受响应时指定utf-8格式

--- src/java/ZLYUtils/Network.java
+++ src/java/ZLYUtils/Network.java
@@ -4,13 +4,16 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 public class Network {
     public static String networkUrl;
+
     public static String sendGet(String url) {
-        if(url==null)throw new IllegalArgumentException("url参数为空");
-        return sendGet(url,"");
+        if (url == null) throw new IllegalArgumentException("url参数为空");
+        return sendGet(url, "");
     }
 
     /**
@@ -21,30 +24,32 @@ public class Network {
      * @return 服务响应内容, null为响应非200
      */
     public static String sendGet(String url, String param) {
-        if(url==null)throw new IllegalArgumentException("url参数为空");
-        if(param==null)throw new IllegalArgumentException("param参数为空");
-        StringBuffer data =  new StringBuffer();;
+        LocalProxy();
+        if (url == null) throw new IllegalArgumentException("url参数为空");
+        if (param == null) throw new IllegalArgumentException("param参数为空");
+        StringBuffer data = new StringBuffer();
+        ;
         BufferedReader in = null;
         String urlStr = url + "?" + param;
-        if(param.equals(""))urlStr = url;
+        if (param.equals("")) urlStr = url;
         try {
             networkUrl = url;
             URL realUrl = new URL(urlStr);
             // 打开和URL之间的连接
             URLConnection connection = realUrl.openConnection();
             // 设置通用的请求属性
+            connection.setRequestProperty("Accept-Encoding", "chunked");
+            connection.setRequestProperty("Charset", "utf-8");
             // 不能使用缓存
             connection.setUseCaches(false);
             // 建立实际的连接
             connection.connect();
-            int getResponseCode  = ((HttpURLConnection) connection).getResponseCode();
+            int getResponseCode = ((HttpURLConnection) connection).getResponseCode();
             if (getResponseCode != 200) {// 检查服务器响应
-                return getResponseCode+"";
+                return getResponseCode + "";
             }
-            // 获取所有响应头字段
-//            Map<String, List<String>> map = connection.getHeaderFields();
             // 定义 BufferedReader输入流来读取URL的响应
-            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            in = new BufferedReader(new InputStreamReader(connection.getInputStream(), "utf-8"));
             String line;
             while ((line = in.readLine()) != null) {
                 data.append(line);
@@ -65,21 +70,22 @@ public class Network {
             }
         }
         return data.toString();
-
     }
+
     /**
      * 向指定 URL 发送POST方法的请求
      * 本方法暂不支持GZIP解压，所以没有设置Accept-Encoding
      * 默认为创新版
      *
-     * @param url    发送请求的 URL
-     * @param param  请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
+     * @param url   发送请求的 URL
+     * @param param 请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
      * @return 所代表远程资源的响应结果
      * @author ZhangLianYu
      */
     public static String sendPost(String url, String param) {
-        if(url==null)throw new IllegalArgumentException("url参数为空");
-        if(param==null)throw new IllegalArgumentException("param参数为空");
+        LocalProxy();
+        if (url == null) throw new IllegalArgumentException("url参数为空");
+        if (param == null) throw new IllegalArgumentException("param参数为空");
         PrintWriter out = null;
         BufferedReader in = null;
         StringBuffer result = new StringBuffer();
@@ -90,6 +96,9 @@ public class Network {
             URLConnection conn = realUrl.openConnection();
             // 设置通用的请求属性
             ((HttpURLConnection) conn).setInstanceFollowRedirects(false);// 302重定向
+            conn.setRequestProperty("Content-Length", String.valueOf(param.length()));
+            conn.setRequestProperty("Accept-Encoding", "chunked");
+            conn.setRequestProperty("Charset", "utf-8");
             // 发送POST请求必须设置如下两行
             conn.setDoOutput(true);
             conn.setDoInput(true);
@@ -101,17 +110,12 @@ public class Network {
             out.flush();
             // 建立实际的连接
             conn.connect();
-            //	StringText encoding = conn.getContentEncoding();// 获取Accept-Encoding编码格式
 //            Map<String, List<String>> map = conn.getHeaderFields();
-            // 遍历所有的响应头字段
-            // for (StringText key : map.keySet()) {
-            // if(map.get("Set-Cookie")==null){break;}
-            //System.out.println(conn.getHeaderField("Set-Cookie"));
-            //setCookies(map);
-            // }
-            int getResponseCode  = ((HttpURLConnection) conn).getResponseCode();
+////            遍历所有的响应头字段
+//            System.out.println(map.get("Charset"));
+            int getResponseCode = ((HttpURLConnection) conn).getResponseCode();
             if (getResponseCode != 200) {// 检查服务器响应
-                return getResponseCode+"";
+                return getResponseCode + "";
             }
 //            if (getResponseCode == 302) {
 //                // 如果会重定向，保存302重定向地址，以及Cookies,然后重新发送请求(模拟请求)
@@ -132,7 +136,7 @@ public class Network {
 //                //	setCookies(map);
 //            }
             // 定义BufferedReader输入流来读取URL的响应
-            in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            in = new BufferedReader(new InputStreamReader(conn.getInputStream(),"utf-8"));
             String line;
             while ((line = in.readLine()) != null) {
                 result.append(line);
@@ -154,13 +158,15 @@ public class Network {
                 ex.printStackTrace();
             }
         }
-        return result.toString();
+            //用新的字符编码生成字符串
+            return result.toString();
+
     }
 
     /**
      * 设置本地代理
      */
-    public static void LocalProxy(){
+    public static void LocalProxy() {
         System.setProperty("http.proxySet", "true"); //将请求通过本地发送
         System.setProperty("http.proxyHost", "127.0.0.1");  //将请求通过本地发送
         System.setProperty("http.proxyPort", "8888"); //将请求通过本地发送
@@ -168,10 +174,11 @@ public class Network {
 
     /**
      * 转码
+     *
      * @param param
      * @return
      */
-    public static String getEncoderString(String param,String encoder){
+    public static String getEncoderString(String param, String encoder) {
         param = param.replace("\n", "\r\n");
         try {
             param = java.net.URLEncoder.encode(param, encoder);
