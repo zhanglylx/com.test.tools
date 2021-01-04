d2e5c6e204e939f86eae6b75db8cb53563242693
zhangly <zhanglylx@163.com>
2021-01-04 10:11:10 +0800

    去除无用代码，加入数据库连接时参数

--- src/java/DataBase/ConnectDataBase.java
+++ src/java/DataBase/ConnectDataBase.java
@@ -42,7 +42,7 @@ public class ConnectDataBase {
         if (!url.matches("^.+/.+$"))
             throw new IllegalArgumentException("url格式不正确:" + url);
         try {
-            this.con = DriverManager.getConnection("jdbc:mysql://" + url+"?useSSL=false&autoReconnect=true", username, password);
+            this.con = DriverManager.getConnection("jdbc:mysql://" + url+"?useUnicode=true&characterEncoding=utf-8&useSSL=false&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT%2b8&zeroDateTimeBehavior=CONVERT_TO_NULL&autoReconnect=true", username, password);
         }catch (Exception e){
             e.printStackTrace();
         }
@@ -86,8 +86,21 @@ public class ConnectDataBase {
         PreparedStatement preparedStatement = con.prepareStatement(sql);
         for(Map.Entry<Integer,String> entry : mapStatement.entrySet()){
             preparedStatement.setString(entry.getKey(),entry.getValue());
+            preparedStatement.close();
+        }
+        try {
+
+        }finally {
+
         }
         return preparedStatement.executeUpdate();
+    }
+
+    public static void main(String[] args) {
+        String str1 = "abc";
+        String str2 = "abc";
+        System.out.println(str1.indexOf(str2));
+        System.out.println(str1.compareTo(str2));
     }
 
     public PreparedStatement getPreparedStatement(String sql,Map<Integer, String> mapStatement) throws SQLException {
@@ -140,63 +153,5 @@ public class ConnectDataBase {
 
     }
 
-
-    public static void main(String[] args) {
-        //声明Connection对象
-        Connection con;
-        //驱动程序名
-        String driver = "com.mysql.jdbc.Driver";
-        //URL指向要访问的数据库名mydata
-        String url = "jdbc:mysql://192.168.1.246:3306/freezwsc";
-        //MySQL配置时的用户名
-        String user = "root_rw";
-        //MySQL配置时的密码
-        String password = "loto5522";
-        //遍历查询结果集
-        try {
-            //加载驱动程序
-            Class.forName(driver);
-            //1.getConnection()方法，连接MySQL数据库！！
-            con = DriverManager.getConnection(url, user, password);
-            if (!con.isClosed())
-                System.out.println("Succeeded connecting to the Database!");
-            //2.创建statement类对象，用来执行SQL语句！！
-            Statement statement = con.createStatement();
-            //要执行的SQL语句
-            String sql = "select * from freeadrelease";
-            //3.ResultSet类，用来存放获取的结果集！！
-            ResultSet rs = statement.executeQuery(sql);
-            System.out.println("-----------------");
-            System.out.println("执行结果如下所示:");
-            System.out.println("-----------------");
-            System.out.println("姓名" + "\t" + "职称");
-            System.out.println("-----------------");
-
-            String job = null;
-            String id = null;
-            while (rs.next()) {
-                //获取stuname这列数据
-                job = rs.getString("adNo");
-                //获取stuid这列数据
-                id = rs.getString("id");
-                //输出结果
-                System.out.println(id + "\t" + job);
-            }
-            rs.close();
-            con.close();
-        } catch (ClassNotFoundException e) {
-            //数据库驱动类异常处理
-            System.out.println("Sorry,can`t find the Driver!");
-            e.printStackTrace();
-        } catch (SQLException e) {
-            //数据库连接失败异常处理
-            e.printStackTrace();
-        } catch (Exception e) {
-            // TODO: handle exception
-            e.printStackTrace();
-        } finally {
-            System.out.println("数据库数据成功获取！！");
-        }
-    }
 
 }
